02669e83f5f0e1eefc66d2684bb9b81f5fa19f9c
Caleb Lui-Yee <113129278+Waluiyeegi@users.noreply.github.com>
2023-09-13 19:51:39 -0400

    added nicks stuff

---
+++ Lec2/zuul_main.java
@@ -0,0 +1,10 @@
+import java.security.PublicKey;
+
+public class zuul_main {
+    public static void main (String[] args){
+        Game zuulGame = new Game();
+        zuulGame.play();
+
+    }
+
+}
